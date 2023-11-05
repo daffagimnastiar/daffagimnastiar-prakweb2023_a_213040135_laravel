5050375e1cc0392d49239ee97941e1ae9fea6ce6
daffagimnastiar <dhafapoex123@gmail.com>
2023-11-05 21:38:45 +0700

    update 11

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
-        'posts' => $category->posts,
-        'category' => $category->name
+        'posts' => $category->posts->load('category', 'author')
     ]);
 });
 
